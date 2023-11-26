49ab5cb99617f88614f8fd295d2f32d903355318
Atchaya <atchayar2020csbs@gmail.com>
2023-11-26 11:42:35 +0530

    Guvi

---
+++ profile.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-header">
+            <h5 class="card-title">User Profile</h5>
+        </div>
+        <div class="card-body">
+            <p class="card-text" id="email"></p>
+            <p class="card-text" id="dob"></p>
+            <p class="card-text" id="age"></p>
+            <p class="card-text" id="contactNumber"></p>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="profile.js"></script>
+</body>
+</html>
